e2d6550c724f073ff16c3770e775e91e136289c5
Adriano Felix <milicofelix@gmail.com>
2020-04-03 16:43:46 -0300

    Category CRUD module finished

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,10 +1,29 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name','slug'];
+    use Sluggable;
+
+    protected $fillable = ['name','active'];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 }
