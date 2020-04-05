0a3399fecbef647297a8bf9512a1f42bd200af7d
Adriano Felix <milicofelix@gmail.com>
2020-04-05 20:38:27 -0300

    Serealize category and product data

--- app/Http/Controllers/Api/ProductCategoryController.php
+++ app/Http/Controllers/Api/ProductCategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\ProductCategoryRequest;
+use App\Http\Resources\ProductCategoryResource;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Collection;
@@ -13,7 +14,7 @@ class ProductCategoryController extends Controller
 {
     public function index(Product $product)
     {
-        return $product->categories;
+        return new ProductCategoryResource($product);
     }
 
     public function store(ProductCategoryRequest $request, Product $product)
@@ -26,7 +27,7 @@ class ProductCategoryController extends Controller
         /** @var Collection $categories */
         $categories = Category::whereIn('id',$categoriesAttachedId)->get();
 
-        return $categories->count() ? response()->json($product,201): [];
+        return $categories->count() ? response()->json(new ProductCategoryResource($product),201): [];
     }
 
     public function destroy(Product $product, Category $category)
