cdabd1b841df242a6b44e3793a889b87dc8eb588
Adriano Felix <milicofelix@gmail.com>
2020-04-11 14:25:50 -0300

    ProductInput CRUD finished

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\ProductInput;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        ProductInput::created(function($input){
+            $product = $input->product;
+            $product->stock += $input->amount;
+            $product->save();
+        });
     }
 }
