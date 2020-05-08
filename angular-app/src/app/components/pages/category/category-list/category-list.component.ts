d4dc188dbb0791b40ad05571b3572996533b7233
Adriano Felix <milicofelix@gmail.com>
2020-05-08 00:09:35 -0300

    pagination ajax implemented

--- angular-app/src/app/components/pages/category/category-list/category-list.component.ts
+++ angular-app/src/app/components/pages/category/category-list/category-list.component.ts
@@ -16,7 +16,11 @@ import {CategoryDeleteService} from "./category-delete.service";
 export class CategoryListComponent implements OnInit {
 
   categories:Array<Category> = [];
-  page = 1;
+  pagination = {
+      page : 1,
+      totalItems: 0,
+      itemsPerPage: 15
+}
 
   @ViewChild(CategoryNewModalComponent, {static: true})
   categoryNewModal: CategoryNewModalComponent;
@@ -44,9 +48,16 @@ export class CategoryListComponent implements OnInit {
   }
 
     getCategories(){
-      this.categoryHttp.list()
+      this.categoryHttp.list(this.pagination.page)
       .subscribe(response => {
           this.categories = response.data
+          this.pagination.totalItems = response.meta.total
+          this.pagination.itemsPerPage = response.meta.perPage
       });
+    }
+
+    pageChanged(page){
+      this.pagination.page = page;
+      this.getCategories();
     }
 }
--- angular-app/src/app/services/http/category-http.service.ts
+++ angular-app/src/app/services/http/category-http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {Category} from "../../models";
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
@@ -13,13 +13,17 @@ export class CategoryHttpService {
 
   constructor(private http: HttpClient) { }
 
-  list():Observable<{ data:Array<Category> }>{
-
+  list(page:number):Observable<{ data:Array<Category>, meta:any }>{
+      const params = new HttpParams({
+          fromObject:{
+              page : page+''
+          }
+      });
       const token = window.localStorage.getItem('token');
       return this.http
-          .get<{ data:Array<Category> }>
+          .get<{ data:Array<Category>, meta:any }>
           (this.baseUrl,{
-
+              params,
               headers:{
                   'Authorization':`Bearer ${token}`
               }
